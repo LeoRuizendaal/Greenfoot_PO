fac70139418d60f02c7d1bc67a64a6e2b8f81858
LeoRuizendaal <leoruizendaal@hotmail.com>
2018-10-02 21:24:53 +0200

    added timers

--- Sokoban/Man.java
+++ Sokoban/Man.java
@@ -1,5 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.util.*;
 /**
  * A man can be moved around with the arrow-keys.
  * A man can push a crate, but only one at a time.
@@ -12,14 +12,18 @@ public class Man extends BlockActor
     private GreenfootImage imageRight;
     private GreenfootImage imageDown;
     private GreenfootImage imageUp;
-    
+
     // Field: the current direction of the man
     // the the static fields in BlockActor for definition
     private int direction;
-    
+
     // Field: the number of steps taken so far.
     protected int nrOfSteps;
-    
+    private boolean IsPowerUp = false;
+    private boolean IsPoisoned = false;
+    private int PoisonTimeout = 0;
+    private int PowerUpTimeout = 0;
+
     /**
      * Constructor. Make sure all images (up, down, left, right) are opened.
      * Initial direction is down
@@ -31,16 +35,16 @@ public class Man extends BlockActor
         imageUp = new GreenfootImage("me_strip4_2.png");
         setImage(imageDown);
         direction = DOWN;
-        nrOfSteps = 0;    
+        nrOfSteps = 0;
     }
-  
+
     /**
      * The player wants to move the man into a certain direction
      * Make sure the man can push a crate, but only one at a time.
      */
     private void wantsToMoveTo (int direction) {
         // hier moet de code komen om het mannetje de kratjes te laten verplaatsen.
-        
+
         if (isFree(direction)) {
             moveTo(direction);
             nrOfSteps++;
@@ -56,20 +60,22 @@ public class Man extends BlockActor
         }
        //moveTo(direction);
        showNumberOfSteps ();
-    }    
-    
+    }
+
     /**
      * Act-method. Check if one of the arrow-keys is pressed
      * Then check if the all crates are in place.
      */
-    public void act() 
+    public void act()
     {
        checkKeypress();
        checkGewonnen();
-    } 
+       checkTouching();
+       Timers();
+    }
 
     /**
-     * Check to see if all crates are in place. 
+     * Check to see if all crates are in place.
      */
     private void checkGewonnen(){
         ((SokoBanWorld)getWorld()).endOfLevel();
@@ -80,7 +86,7 @@ public class Man extends BlockActor
      */
     private void showNumberOfSteps (){
         getWorld().showText ("Aantal stappen = " + nrOfSteps, 3, 0);
-    }    
+    }
 
     /**
      * Select the right image of the man based on the direction
@@ -101,9 +107,9 @@ public class Man extends BlockActor
                 break;
             default:
                 break;
-        }     
+        }
     }
-    
+
    /**
     * Check to see if the key is pressed
     */
@@ -122,7 +128,7 @@ public class Man extends BlockActor
         if (Greenfoot.isKeyDown("down")) {
            newDirection = DOWN;
         }
-        
+
         // if one of the arrow keys has been pressed
         if (newDirection != -1) {
             // if the man was already in the same direction
@@ -133,9 +139,46 @@ public class Man extends BlockActor
             // the man wants to move in a new direction
             // change the image of the man
             else {
-                setImage(newDirection);                
+                setImage(newDirection);
                 direction = newDirection;
             }
         }
+   }
+
+   private void checkTouching(){
+     List<PowerUp> PowerUps = getIntersectingObjects(PowerUp.class);
+     List<Bee> Bees = getIntersectingObjects(Bee.class);
+     SokoBanWorld world = (SokoBanWorld) getWord();
+     if ((PowerUps.size > 0) && (!IsPowerUp)) {
+       PowerUp TPowerUp = PowerUps.get(0);
+       world.removeObject(TPowerUp);
+       IsPowerUp = true;
+       PowerUpTimeout = 50;
+     }
+     if (Bees.size > 0) {
+       Bee TBee = Bees.get(0);
+       if (IsPowerUp){
+         world.removeObject(TBee);
+       } else {
+         IsPoisoned = true;
+         PoisonTimeout = 50;
+       }
+     }
+   }
+
+   private void Timers(){
+      if (PowerUpTimeout > 0) {
+        PowerUpTimeout--;
+      } else if (PowerUpTimeout == 0){
+        IsPowerUp = false;
+      }
+      if (IsPoisoned) {
+        if (PoisonTimeout > 0) {
+            //removeHealth(1);
+            PoisonTimeout--;
+        } else {
+          IsPoisoned = false;
+        }
+      }
    }
  }
